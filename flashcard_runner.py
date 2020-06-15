c08060cc3e76686bae0081ba06e9cfdeb89c0da1
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-15 16:42:21 -0600

    Refactor flashcard_runner to use interface start method

--- flashcard_runner.py
+++ flashcard_runner.py
@@ -3,10 +3,6 @@ from lib.deck import Deck
 from lib.round import Round
 from lib.turn import Turn
 from lib.interface import Interface
+from lib.card_generator import CardGenerator
 
-card1 = Card("Geography", "What is the capital of Alaska? ", "Juneau")
-card2 = Card("STEM", "The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet? ", "Mars")
-card3 = Card("STEM", "Describe in words the exact direction that is 697.5° clockwise from due north? ", "North north west")
-deck = Deck([card1, card2, card3])
-interface = Interface(deck)
-interface.welcome()
+Interface.start()
